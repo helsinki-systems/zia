3f23a6462516afcc9d54b4808ef21ca3899a7146
Simon Hauser <simon.hauser@helsinki-systems.de>
2024-10-22 10:13:07 +0200

    chore: add dev shell

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
           pkgs = (import nixpkgs) {
             inherit system;
           };
+          inherit (pkgs) rustPackages;
         in
         {
           packages = {
@@ -21,6 +22,25 @@
             zia-server = pkgs.callPackage ./derivation.nix {
               cargoToml = ./zia-server/Cargo.toml;
             };
+          };
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              # Compiler and linker
+              rustPackages.rustc
+              clang_15
+              llvmPackages.lld
+              # Native dependencies
+              pkg-config
+              rustPackages.cargo
+              # Utilities
+              cargo-deny
+              cargo-outdated
+              rustPackages.clippy
+              rustPackages.rustfmt
+            ];
+
+            RUST_SRC_PATH = "${rustPackages.rustPlatform.rustLibSrc}";
+            CI_COMMIT_SHA = builtins.getEnv "CI_COMMIT_SHA";
           };
         }
       ) // {
